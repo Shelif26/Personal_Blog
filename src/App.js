98eec0f32aaad5636e2e3880c09e0931f17455e0
M-A-Shelif <shelifshelif30@gmail.com>
2023-01-28 21:34:09 +0530

    border

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import "./App.css"
 
 function App() {
   return (
-    <div>
+    <div className="appdiv">
       <Header />
       <Footer />
     </div>
