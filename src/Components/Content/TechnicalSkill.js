4d89aa4a6fa21358388c47d7002763b570c3e588
Shelif26 <shelifshelif11@gmail.com>
2023-10-24 23:05:33 +0530

    fixed the demo look

--- src/Components/Content/TechnicalSkill.js
+++ src/Components/Content/TechnicalSkill.js
@@ -83,9 +83,7 @@ const TechnicalSkill = () => {
                 <div>
                   <p className={classes.H4}>The Food Ordering App</p>
                   <p className={classes.ProjectDescription}>
-                    This is a reactive food ordering web application.This is
-                    just the frame of a basic food ordering app with some dishes
-                    preset inside.
+                    This is a reactive food ordering web application is a digital platform designed to facilitate the process of ordering food from a restaurant or food establishment. The term "reactive" suggests that the application responds dynamically to user input and updates in real-time, providing a seamless and interactive user experience.
                   </p>
                   <img
                     className={classes.eximg1}
@@ -96,9 +94,7 @@ const TechnicalSkill = () => {
                     src={require("../Content/ImagesCombo/food2.png")}
                   />
                   <p className={classes.ProjectDescription}>
-                    This is a sample that we can place order for which we would
-                    like to have by selecting the particular food item also can
-                    add the count.
+                    You can place an order for a sample of the food item you'd like and specify the quantity you'd like to add.
                   </p>
                   <p className={classes.ProjectDescription}>
                     While adding the count of food items the prize of the
@@ -163,12 +159,6 @@ const TechnicalSkill = () => {
           )}
           {isEditing2 && (
             <>
-              <button
-                onClick={stopEditingHandler2}
-                className={classes.formbutton}
-              >
-                <span>Collapse</span>
-              </button>
               <motion.div
                 initial={{ opacity: 0, rotateY: -180 }}
                 animate={{ opacity: 1, rotateY: 0 }}
@@ -177,7 +167,9 @@ const TechnicalSkill = () => {
               >
                 <div>
                   <p className={classes.H4}>The Expense App</p>
-                  <p className={classes.ProjectDescription}></p>
+                  <p className={classes.ProjectDescription}>
+                    The primary purpose of this web app is to simplify the process of expense tracking, making it convenient and efficient. Users can easily input and categorize expenses, including various transactions such as grocery shopping, dining out, utility bills, entertainment, and much more. By recording each expense in a systematic manner, users gain a clearer understanding of how they allocate their money, which in turn promotes better financial decision-making.
+                  </p>
                   <img
                     className={classes.eximg1}
                     src={require("../Content/ImagesCombo/expense3.png")}
@@ -186,59 +178,33 @@ const TechnicalSkill = () => {
                     className={classes.eximg4}
                     src={require("../Content/ImagesCombo/expense1.png")}
                   />
-                </div>
-              </motion.div>
-            </>
-          )}
-        </div>
-
-        {/* ------------------------------------------------------------------ */}
-        <div className={classes.Project}>
-          <img
-            className={classes.projectimg}
-            alt="reactform"
-            src={require("./ImagesCombo/react-forms.gif")}
-          />
-          {!isEditing3 && (
-            <>
-              <motion.div
-                initial={{ opacity: 0, rotateY: -180 }}
-                animate={{ opacity: 1, rotateY: 0 }}
-                exit={{ opacity: 0, rotateY: 180 }}
-                transition={{ duration: 0.5 }}
-              >
-                <div className={classes.ProjectChild}>
-                  <p className={classes.H4}>The React Black Form</p>
                   <p className={classes.ProjectDescription}>
-                    A very basic model form has no background operation only UI.
+                  Users can categorize their expenses into different categories like food, transportation, entertainment, housing, and others. This helps in organizing and analyzing spending patterns
                   </p>
-                </div>
-              </motion.div>
-              <button
-                onClick={startEditingHandler3}
-                className={classes.formbutton}
-              >
-                <span>Expand</span>
-              </button>
-            </>
-          )}
-          {isEditing3 && (
-            <>
-              <button
-                onClick={stopEditingHandler3}
-                className={classes.formbutton}
-              >
-                <span>Collapse</span>
-              </button>
-              <motion.div
-                initial={{ opacity: 0, rotateY: -180 }}
-                animate={{ opacity: 1, rotateY: 0 }}
-                exit={{ opacity: 0, rotateY: 180 }}
-                transition={{ duration: 0.5 }}
-              >
-                <div>
-                  <p className={classes.H4}>The Food Ordering App</p>
-                  <p className={classes.ProjectDescription}></p>
+                  <p className={classes.ProjectDescription}>
+                  At the end of the year, the app can generate a comprehensive summary report that breaks down the total expenses for each category and provides insights into annual spending trends.
+                  </p>
+                  <img
+                    className={classes.eximg1}
+                    src={require("../Content/ImagesCombo/expense4.png")}
+                  />
+                  <img
+                    className={classes.eximg4}
+                    src={require("../Content/ImagesCombo/expense2.png")}
+                  />
+                  <div className={classes.buttonDiv}>
+                    <button className={classes.demoButton}>
+                      <a href="https://react-family-app-shelif26.vercel.app/">
+                        Have a Look
+                      </a>
+                    </button>
+                    <button
+                      onClick={stopEditingHandler2}
+                      className={classes.demoButton}
+                    >
+                      Collapse
+                    </button>
+                  </div>
                 </div>
               </motion.div>
             </>
